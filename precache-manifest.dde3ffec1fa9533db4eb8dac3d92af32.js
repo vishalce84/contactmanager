4a40eff1192ae88f0ede77bb091b375388405449
vishalce84 <63300242+vishalce84@users.noreply.github.com>
2020-04-08 15:10:08 +0530

    Updates

---
+++ precache-manifest.dde3ffec1fa9533db4eb8dac3d92af32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77183c5959ad8bea6422291dbdb31787",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "e1f042599d95dd9b4f2d",
+    "url": "/contactmanager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d1906ebce740529d9242",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "e1f042599d95dd9b4f2d",
+    "url": "/contactmanager/static/js/2.78b29e3d.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/contactmanager/static/js/2.78b29e3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1906ebce740529d9242",
+    "url": "/contactmanager/static/js/main.1882b3bc.chunk.js"
+  },
+  {
+    "revision": "d86057960233a7428d99",
+    "url": "/contactmanager/static/js/runtime-main.2ce66dc9.js"
+  }
+]);
